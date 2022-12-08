3b8b09417009838ed7b30f072251037e0af9b250
revelations? <flfkflfkkf@gmail.com>
2022-12-08 11:40:47 +0200

    Добавьте файлы проекта.

---
+++ 2zzadanie/2zzadanie.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(0, ".1251");
+	double x, y;
+	x = (2 * 2 * 3 * cos(45) * 5);
+	y = ((2 + 3 - 2.5) / 2);
+	cout << "Бісектриса " << x << endl;
+	cout << "Радіус " << y << endl;
+	system("pause>>void");
+	return 0;
+}
